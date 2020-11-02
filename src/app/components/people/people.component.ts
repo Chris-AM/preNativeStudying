febcf32f574231b626552a8fffe9f698d0fdf451
Chris-AM <c.aranguizm@outlook.com>
2020-11-01 23:29:03 -0300

    intput output explanation added

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -6,5 +6,9 @@ import { Component, Input } from '@angular/core';;
   styleUrls: ['./people.component.css']
 })
 export class PeopleComponent {
+  //@Input() and @Output() allow Angular to share data 
+  //between the parent context and child directives or 
+  //components.An @Input() property is writable while an 
+  //@Output() property is observable.
   @Input() peopleList: String[]
 }
