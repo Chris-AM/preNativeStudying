25306f111180d279c7cf12d121b61f2e31a9cf3a
Chris-AM <c.aranguizm@outlook.com>
2020-11-01 23:07:56 -0300

    routing module and why it's used added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PeopleInputComponent } from './components/people-input/people-input.component';
+import { PeopleComponent } from './components/people/people.component';
+
+const ROUTES: Routes = [
+  { path: '', component: PeopleComponent },
+  { path: 'input', component: PeopleInputComponent }
+];
 
 @NgModule({
-
+  //Import the RouterModule:
+  //It's important to import the RouterModule to tell angular about our routes
+  //forRoot:
+  //Creates and configures a module with all the router providers
+  //and directives.Optionally sets up an application listener to perform 
+  //an initial navigation.
+  imports: [RouterModule.forRoot(ROUTES)],
+  exports: [RouterModule]
 })
-  
-export class AppRoutingModule{
+
+export class AppRoutingModule {
 
 }
