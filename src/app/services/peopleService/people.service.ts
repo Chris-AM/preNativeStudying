f38af06c0dde1732f0268b189c13d200fd83ae7a
Chris-AM <c.aranguizm@outlook.com>
2020-11-02 02:09:26 -0300

    services added, now we are using router-outlet

---
+++ src/app/services/peopleService/people.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+//@Injectable allows inject services in components with 
+//the dependency inject feature
+@Injectable()
+export class PeopleService {
+
+}
