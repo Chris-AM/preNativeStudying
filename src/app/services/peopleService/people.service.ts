e6bf987748c500957f17f3ead5f0cb0b2dc0d9bd
Chris-AM <c.aranguizm@outlook.com>
2020-11-02 13:29:46 -0300

    injecting HttpClient and API

--- src/app/services/peopleService/people.service.ts
+++ src/app/services/peopleService/people.service.ts
@@ -1,14 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 //@Injectable allows inject services in components with 
 //the dependency inject feature
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PeopleService {
+  actualizedPeople = new Subject<string[]>();
+  people: string[] = [];
 
-  people = ['Christian', 'María', 'Valentina', 'Vicente'];
+  constructor(private http: HttpClient) {
 
-  addPerson(name: string) {
-    this.people.push(name);
-    console.log(this.people);
   }
+
+  fetchPeople() {
+    //the rxjs map receives data and the pipe changes it the way we want
+    this.http.get<any>('https://swapi.dev/api/people')
+      .pipe(map(resData => {
+        //the return is the normal JS map
+        return resData.results.map(character => character.name);
+      })).subscribe(transformedData => { this.actualizedPeople.next(transformedData)
+  });
+}
+
+addPerson(name: string) {
+  this.people.push(name);
+  console.log(this.people);
+  this.actualizedPeople.next(this.people);
+}
+
+removePerson(name: string) {
+  this.people = this.people.filter(person => {
+    return person !== name;
+  })
+  this.actualizedPeople.next(this.people);
+}
+
 }
