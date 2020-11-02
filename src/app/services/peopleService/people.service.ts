0e8eedf6f8942e9f96f5174ff6f85180a9e00ae5
Chris-AM <c.aranguizm@outlook.com>
2020-11-02 03:33:41 -0300

    service created

--- src/app/services/peopleService/people.service.ts
+++ src/app/services/peopleService/people.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@angular/core';
 
 //@Injectable allows inject services in components with 
 //the dependency inject feature
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class PeopleService {
 
+  people = ['Christian', 'María', 'Valentina', 'Vicente'];
+
+  addPerson(name: string) {
+    this.people.push(name);
+    console.log(this.people);
+  }
 }
